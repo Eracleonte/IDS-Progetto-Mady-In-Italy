fe13fe95dd2d1183600106a8dca8a68fc2b554c1
Pietro Villa <pietro.villa@studenti.unicam.it>
2025-01-29 14:47:59 +0100

    init

---
+++ app/src/main/java/it/unicam/cs/ids/Main.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.ids;
+
+import it.unicam.cs.ids.api.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!" + Util.bun());
+    }
+}
